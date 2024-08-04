9f4f0f5d73fb9d36947d91439982d4b65fab3424
Artur Markouski <markouski.artur@student.ehu.lt>
2024-08-04 22:39:21 +0200

    feat: Add Last Tasks

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,8 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        String valid = (firstSide + secondSide > thirdSide && secondSide + thirdSide > firstSide && firstSide + thirdSide > secondSide)?
+                "this is a valid triangle" : "it's not a triangle";
+        System.out.println(valid);
     }
-
 }
